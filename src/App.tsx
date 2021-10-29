5c1182337f2742b21e9c029f87e840c5fadc6b4a
Craig Cronin <craig.cronin@fireeye.com>
2021-10-29 10:34:33 -0400

    Add better mui structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Container } from '@mui/material';
+import { Container, CssBaseline } from '@mui/material';
 import { Provider } from 'react-redux';
 import { CookiesProvider } from 'react-cookie';
-
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -10,15 +10,20 @@ import AppRouter from './components/app-router/app-router.component';
 import CookieCheck from './components/cookie-check/cookie-check.component';
 import store from './redux/store';
 
+const theme = createTheme();
+
 const App = () => {
     return (
         <CookiesProvider>
             <Provider store={store}>
-                <Container disableGutters maxWidth={false}>
-                    <CookieCheck />
-                    <AppRouter />
-                </Container>
-                <ToastContainer />
+                <ThemeProvider theme={theme}>
+                    <Container disableGutters maxWidth={false}>
+                        <CssBaseline />
+                        <CookieCheck />
+                        <AppRouter />
+                    </Container>
+                    <ToastContainer />
+                </ThemeProvider>
             </Provider>
         </CookiesProvider>
     );
